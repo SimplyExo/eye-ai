cc894a4c5ee1a274ed70e5347a42b18432f6dc14
Peanutt42 <p3anutt42@gmail.com>
2025-06-17 10:59:17 +0200

    added option to disable speech recognition in settings

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/MainActivity.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : ComponentActivity() {
 		if (permissionManager.isMicrophonePermissionGranted()) {
 			eyeAIApp()
 				.voskModel
-				.initService(
+				?.initService(
 					::onPartialSpeechRecognitionResult,
 					::onFinalSpeechRecognitionResult,
 					::onSpeechRecognitionLoaded
@@ -91,12 +91,16 @@ class MainActivity : ComponentActivity() {
 		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
 
 		eyeAIApp().updateSettings()
+
+		updateSpeechRecognitionUIVisibility()
 	}
 
 	override fun onResume() {
 		super.onResume()
 
 		eyeAIApp().updateSettings()
+
+		updateSpeechRecognitionUIVisibility()
 
 		permissionManager.requestPermissions()
 		updateUngrantedPermissionsNotice()
@@ -108,27 +112,30 @@ class MainActivity : ComponentActivity() {
 		if (eyeAIApp().cameraManager.cameraPreview == null && cameraPermissionGranted)
 			initCamera()
 
-		if (!eyeAIApp().voskModel.isInitialized() && permissionManager.isMicrophonePermissionGranted()) {
-			eyeAIApp().voskModel.initService(
-				::onPartialSpeechRecognitionResult,
-				::onFinalSpeechRecognitionResult,
-				::onSpeechRecognitionLoaded
-			)
-		}
+		val voskModelInitialized = eyeAIApp().voskModel?.isInitialized() == true
+		if (permissionManager.isMicrophonePermissionGranted() && !voskModelInitialized) {
+			eyeAIApp().voskModel?.apply {
+				initService(
+					::onPartialSpeechRecognitionResult,
+					::onFinalSpeechRecognitionResult,
+					::onSpeechRecognitionLoaded
+				)
 
-		eyeAIApp().voskModel.startListening()
+				startListening()
+			}
+		}
 	}
 
 	override fun onPause() {
 		super.onPause()
 
-		eyeAIApp().voskModel.stopListening()
+		eyeAIApp().voskModel?.stopListening()
 	}
 
 	override fun onDestroy() {
 		super.onDestroy()
 
-		eyeAIApp().voskModel.closeService()
+		eyeAIApp().voskModel?.closeService()
 	}
 
 	private fun onCameraPermissionResult(isGranted: Boolean) {
@@ -141,10 +148,10 @@ class MainActivity : ComponentActivity() {
 	}
 
 	private fun onMicrophonePermissionResult(isGranted: Boolean) {
-		if (isGranted) {
+		if (isGranted && eyeAIApp().settings.enableSpeechRecognition) {
 			eyeAIApp()
 				.voskModel
-				.initService(
+				?.initService(
 					::onPartialSpeechRecognitionResult,
 					::onFinalSpeechRecognitionResult,
 					::onSpeechRecognitionLoaded
@@ -167,12 +174,12 @@ class MainActivity : ComponentActivity() {
 
 		// pause recognition for 500ms after final speech command to prevent mic picking up the vibration sounds
 		if (System.currentTimeMillis() - lastFinalResultMillis > 1000) {
-			eyeAIApp().voskModel.stopListening()
+			eyeAIApp().voskModel?.stopListening()
 
 			// vibrate for 100ms
 			vibrate(eyeAIApp(), 100)
 
-			eyeAIApp().voskModel.startListening()
+			eyeAIApp().voskModel?.startListening()
 
 			lastFinalResultMillis = System.currentTimeMillis()
 		}
@@ -202,6 +209,17 @@ class MainActivity : ComponentActivity() {
 		} else {
 			ungrantedPermissionsNotice!!.visibility = View.VISIBLE
 		}
+	}
+
+	private fun updateSpeechRecognitionUIVisibility() {
+		val visibility = if (eyeAIApp().settings.enableSpeechRecognition) {
+			View.VISIBLE
+		} else {
+			View.GONE
+		}
+
+		speechRecognitionPartialResultText?.visibility = visibility
+		speechRecognitionFinalResultText?.visibility = visibility
 	}
 
 	private fun updateUngrantedPermissionsNotice() {
--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/Settings.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/Settings.kt
@@ -12,6 +12,9 @@ class Settings(val context: Context) {
 	var showProfilingInfo: Boolean
 		private set
 
+	var enableSpeechRecognition: Boolean
+		private set
+
 	init {
 		depthModel = sharedPreferences.getString(
 			context.getString(R.string.depth_model_setting),
@@ -21,6 +24,11 @@ class Settings(val context: Context) {
 		showProfilingInfo = sharedPreferences.getBoolean(
 			context.getString(R.string.show_profiling_info_setting),
 			false
+		)
+
+		enableSpeechRecognition = sharedPreferences.getBoolean(
+			context.getString(R.string.enable_speech_recognition_setting),
+			true
 		)
 	}
 }
