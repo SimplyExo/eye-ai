3b8ead6d003123ccd393cf26b7e721fd64de0e86
Peanutt42 <p3anutt42@gmail.com>
2025-07-18 18:41:46 +0200

    removed kotlin tensorflowlite dependency + faster input image conversion

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/object_detection/YoloModel.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/object_detection/YoloModel.kt
@@ -3,25 +3,15 @@ package com.algorithmic_alliance.eyeaiapp.object_detection
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
 import android.os.Build
 import android.os.SystemClock
 import com.algorithmic_alliance.eyeaiapp.NativeLib
 import java.io.File
-import org.tensorflow.lite.DataType
-import org.tensorflow.lite.Interpreter
-import org.tensorflow.lite.support.common.ops.CastOp
-import org.tensorflow.lite.support.common.ops.NormalizeOp
-import org.tensorflow.lite.support.image.ImageProcessor
-import org.tensorflow.lite.support.image.TensorImage
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import androidx.core.graphics.scale
 
 class YoloModel(var info: YoloModelInfo) {
-	private var interpreter: Interpreter? = null
 	private lateinit var labels: Array<String>
 
 	private var tensorWidth = 0
@@ -29,57 +19,30 @@ class YoloModel(var info: YoloModelInfo) {
 	private var numChannel = 0
 	private var numElements = 0
 
-	private val imageProcessor = ImageProcessor.Builder()
-		.add(NormalizeOp(INPUT_MEAN, INPUT_STANDARD_DEVIATION))
-		.add(CastOp(INPUT_IMAGE_TYPE))
-		.build()
-
-	private val paint_box = Paint().apply {
-		color = Color.RED
-		style = Paint.Style.STROKE
-		strokeWidth = 2f
-	}
-
-	val paint_text = Paint().apply {
-		color = Color.RED
-		strokeWidth = 1f
-	}
-
-	fun create(context: Context)
-	{
+	fun create(context: Context) {
 		// Erstellen einer Yolo-Instanz
 		val modelBytes = info.getAsBytes(context)
 		labels = info.readLinesFromAsset(context, "coco.names")
 
-		NativeLib.initYoloRuntime(modelBytes, labels,
+		NativeLib.initYoloRuntime(
+			modelBytes, labels,
 			createSerializedGpuDelegateCacheDirectory(context).path,
-			getModelToken(context, info.filename))
+			getModelToken(context, info.filename)
+		)
 
 		val model = ByteBuffer.allocateDirect(modelBytes.size)
 			.order(ByteOrder.nativeOrder())
 		model.put(modelBytes)
 		model.rewind()
 
-		val options = Interpreter.Options()
-		options.numThreads = 4
-		interpreter = Interpreter(model, options)
-
-		val inputShape = interpreter?.getInputTensor(0)?.shape() ?: return
-		val outputShape = interpreter?.getOutputTensor(0)?.shape() ?: return
-
-		tensorWidth = inputShape[1]
-		tensorHeight = inputShape[2]
-		numChannel = outputShape[1]
-		numElements = outputShape[2]
-	}
-
-	fun clear() {
-		interpreter?.close()
-		interpreter = null
+		// TODO: fetch from input/output shape in c++
+		tensorWidth = 640
+		tensorHeight = 640
+		numChannel = 84
+		numElements = 8400
 	}
 
 	fun runInference(frame: Bitmap): Array<BoundingBox>? {
-		interpreter ?: return null
 		if (tensorWidth == 0) return null
 		if (tensorHeight == 0) return null
 		if (numChannel == 0) return null
@@ -89,30 +52,14 @@ class YoloModel(var info: YoloModelInfo) {
 
 		val resizedBitmap = frame.scale(tensorWidth, tensorHeight, false)
 
-		val tensorImage = TensorImage(DataType.FLOAT32)
-		tensorImage.load(resizedBitmap)
+		val input = NativeLib.bitmapToRgbHwc255FloatArray(resizedBitmap)
 
-        val buffer = tensorImage.buffer
-        buffer.rewind()
-
-        val floatArray = FloatArray(buffer.remaining() / 4)
-        buffer.asFloatBuffer().get(floatArray)
-
-		val output = NativeLib.runYoloOperation(floatArray, numElements, numChannel);
+		val output = NativeLib.runYoloOperation(input, numElements, numChannel)
 
 		val bestBoxes = output
 		inferenceTime = SystemClock.uptimeMillis() - inferenceTime
 
 		return bestBoxes
-	}
-
-	companion object {
-		private const val INPUT_MEAN = 0f
-		private const val INPUT_STANDARD_DEVIATION = 255f
-		private val INPUT_IMAGE_TYPE = DataType.FLOAT32
-		private val OUTPUT_IMAGE_TYPE = DataType.FLOAT32
-		private const val CONFIDENCE_THRESHOLD = 0.5F
-		private const val IOU_THRESHOLD = 0.5F
 	}
 
 	fun createSerializedGpuDelegateCacheDirectory(context: Context): File {
