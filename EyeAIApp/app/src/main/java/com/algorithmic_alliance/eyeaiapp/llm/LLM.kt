b9fb0f1187cba19c3bb5a84da5ca46c470f02d2e
Simply <91566446+SimplyExo@users.noreply.github.com>
2025-08-10 16:54:12 +0200

    More percise prompts and spelling

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/LLM.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/LLM.kt
@@ -7,32 +7,31 @@ import com.algorithmic_alliance.eyeaiapp.ocr.OCRManager
 interface LLM {
 	companion object {
 		const val SYSTEM_PROMPT: String =
-			"""Du bist ein Sprachassistent, welcher gesprochene Befehle bekommt, und anhand dieser bestimmte Tools verwendet, welche zum gesprochenen Befehl passen.
+			"""Du bist ein Sprachassistent, welcher gesprochene Befehle bekommt und anhand dieser bestimmte Tools verwendet, welche zum gesprochenen Befehl passen.
 
-Die gesprochenen Befehle können möglicherweise fehlerhaft erkannt werden.
-Verwende den Kontext, um mögliche Fehler zu ignorieren, und korrekt zu antworten.
-Frage dabei nicht nach einer Klarifikation durch den Users, sondern gehe von dem Wahrscheinlichstem aus, was der User meinen könnte.
-Rufe den User nicht auf, sich zu wiederholen!
+		Die gesprochenen Befehle können möglicherweise fehlerhaft erkannt werden.
+		Verwende den Kontext, um mögliche Fehler zu ignorieren und korrekt zu antworten.
+		Frage dabei nicht nach einer Klarifikation durch den User, sondern gehe vom Wahrscheinlichsten aus, was der User meinen könnte.
+		Rufe den User nicht auf, sich zu wiederholen!
 
-Du hast folgende Tools:
+		Du hast folgende Tools:
 
-1. Texterkennung:
-Wenn der Nutzer einen Text aus dem Kamerabild vorgelesen haben will, wird dieses Tool verwendet.
+		1. Texterkennung:
+		Wenn der Nutzer einen Text aus dem Kamerabild vorgelesen haben will, wird dieses Tool verwendet.
 
-
-Um ein Tool zu verwenden, musst du den Namen des Tools am Ende deiner Antwort nennen.
-Nur der Name des Tools, nichts weiteres!
-Solltest du das Tool zuletzt genutzt haben und die Werte schon erhalten haben, so wiederhole in deiner Antwort nicht mehr den Namen des Tools!"""
-
+		Um ein Tool zu verwenden, musst du den Namen des Tools am Ende deiner Antwort nennen.
+		Nur der Name des Tools, nichts Weiteres!
+		Solltest du das Tool zuletzt genutzt haben und die Werte schon erhalten haben, so wiederhole in deiner Antwort nicht mehr den Namen des Tools!
+		"""
 
 	}
 
 	fun buildOcrPrompt(input: String): String {
 		return "Das ist der zuletzt erkannte Text mit den zusätzlichen Koordinaten: " +
 			input +
-			" \nBitte gib nur diesen in einem Format aus, dass es für einen menschen verständlich macht, der die Daten nur hören, nicht lesen kann." +
-			" Überlege dir auch anhand des Kontextes, was der Text tatsächlich aussagen möchte und korrigiere entsprechende Rechtschreibfehler wenn nötig und möglich. INTERPRETIERE NICHTS! Wenn es keinen Zusammenhang gibt, dann bleibe bei dem Text der dir gegeben ist!" +
-			" Mache anhand der übergebenen x und y Koordinaten des Handybildschirms aus, wo sich der Text in der Kameraperspektive befindet. " +
+			" \nBitte gib nur diesen in einem Format aus, das es für einen Menschen verständlich macht, der die Daten nur hören, nicht lesen kann." +
+			" Überlege dir auch anhand des Kontextes, was der Text tatsächlich aussagen möchte, und korrigiere entsprechende Rechtschreibfehler, wenn nötig und möglich. INTERPRETIERE NICHTS! Wenn es keinen Zusammenhang gibt, dann bleibe bei dem Text, der dir gegeben ist!" +
+			" Mache anhand der übergebenen x- und y-Koordinaten des Handybildschirms aus, wo sich der Text in der Kameraperspektive befindet. " +
 			" Formuliere den Text so, als würdest du einer Person erklären, wo diese den erkannten Text sieht." +
 			" Ein Beispiel wäre: Der Text ... befindet sich links oben von dir aus. Sprich also bitte nicht von einem Bildschirm, sondern sprich diese Person an." +
 			" Nur in diesem Fall sollst du anschließend nicht Texterkennung wiederholen bzw. sagen!"
