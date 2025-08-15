f34fbea444a108f4f7e6a2a25c6b3ad46a186625
Simply <91566446+SimplyExo@users.noreply.github.com>
2025-08-15 21:19:35 +0200

    Adding the settings prompt

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/LLM.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/LLM.kt
@@ -18,13 +18,34 @@ interface LLM {
 
 		1. Texterkennung:
 		Wenn der Nutzer einen Text aus dem Kamerabild vorgelesen haben will, wird dieses Tool verwendet.
+	
+		2. Einstellungen:
+		Wenn der Nutzer die Einstellungen der Text-zu-Sprache-Instanz anpassen möchte, wie beispielsweise die Lautstärke oder aber die Sprechgeschwindigkeit, so wird dieses Tool verwendet.
+	
 
 		Um ein Tool zu verwenden, musst du den Namen des Tools am Ende deiner Antwort nennen.
 		Nur der Name des Tools, nichts Weiteres!
 		Solltest du das Tool zuletzt genutzt haben und die Werte schon erhalten haben, so wiederhole in deiner Antwort nicht mehr den Namen des Tools!
 		"""
 
+		const val SETTINGS_PROMPT: String = """
+			Der Nutzer möchte die Einstellungen anpassen. Er hat folgende Möglichkeiten: 
+			1) Sprachgeschwindigkeit der Sprachausgabe anpassen
+			2) Assistentenstimme anpassen
+			3) Lautstärke anpassen
+			4) Er kann die Einstellungen verlassen
+			
+			Erkläre dich einverstanden damit und diktiere ihm diese bitte in dieser Reihenfolge, reihe diese bitte mit erstens, zweitens, drittens und viertens aneinander.
+			Frage ihn dann, welche Option er wählen möchte.
+			Merke dir, das der Nutzer soeben die Einstellungen aufgerufen hat. Nennt der Nutzer im nächsten Befehl eine dieser Optionen, so schreibe nur die Zahl der Option als deine nächste Antwort.
+			Bei "Sprachgeschwindigkeit der Sprachausgabe anpassen" wäre dies also 1, bei Lautstärke anpassen die 3.
+			In diesem Fall sollst du anschließend nicht Einstellungen wiederholen bzw. sagen!
+		"""
+
 	}
+
+
+
 
 	fun buildOcrPrompt(input: String): String {
 		return "Das ist der zuletzt erkannte Text mit den zusätzlichen Koordinaten: " +
@@ -34,8 +55,9 @@ interface LLM {
 			" Mache anhand der übergebenen x- und y-Koordinaten des Handybildschirms aus, wo sich der Text in der Kameraperspektive befindet. " +
 			" Formuliere den Text so, als würdest du einer Person erklären, wo diese den erkannten Text sieht." +
 			" Ein Beispiel wäre: Der Text ... befindet sich links oben von dir aus. Sprich also bitte nicht von einem Bildschirm, sondern sprich diese Person an." +
-			" Nur in diesem Fall sollst du anschließend nicht Texterkennung wiederholen bzw. sagen!"
+			" In diesem Fall sollst du anschließend nicht Texterkennung wiederholen bzw. sagen!"
 	}
 
-	suspend fun generate(prompt: String): String
+
+	fun generate(command: String, structured: Boolean): String
 }
