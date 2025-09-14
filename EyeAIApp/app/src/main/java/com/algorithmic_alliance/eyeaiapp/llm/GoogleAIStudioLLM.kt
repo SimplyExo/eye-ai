37e29aea1297f9ef86b5b90f9d71f0f981c6cf80
Simply <91566446+SimplyExo@users.noreply.github.com>
2025-09-14 13:59:17 +0200

    Using gemini-2.5-flash instead of gemini-2.5-flash-lite

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/GoogleAIStudioLLM.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/llm/GoogleAIStudioLLM.kt
@@ -22,7 +22,7 @@ import javax.net.ssl.X509TrustManager
 
 class GoogleAIStudioLLM(private val apiKey: String, private val customEndpoint: String?) : LLM {
 	companion object {
-		const val MODEL_NAME: String = "gemini-2.5-flash-lite"
+		const val MODEL_NAME: String = "gemini-2.5-flash"
 		private const val GOOGLE_GEN_AI_ENDPOINT = "https://generativelanguage.googleapis.com"
 	}
 
