af9b0f25c0980f27e4f08115ce843d5beaea326c
coookie0 <Lars_K_Holm@web.de>
2026-07-12 19:45:11 +0200

    Added WelcomePage.kt, which displays the starting page of the app

--- EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/UI/WelcomePage.kt
+++ EyeAIApp/app/src/main/java/com/algorithmic_alliance/eyeaiapp/UI/WelcomePage.kt
@@ -1,25 +1,60 @@
 package com.algorithmic_alliance.eyeaiapp.UI
 
+import com.algorithmic_alliance.eyeaiapp.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.clearAndSetSemantics
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun WelcomePage(modifier: Modifier = Modifier){
-    Surface(modifier = modifier, color = MaterialTheme.colorScheme.background) { Text(text = "EyeAI", modifier = modifier.padding(24.dp))}
+fun WelcomePage(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.surface
+    ) {
+        Column(
+            modifier = Modifier.padding(24.dp), verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(painter = painterResource(R.drawable.ic_launcher_web), contentDescription = "App-Logo")
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+                    .semantics { contentDescription = "Startet die Eye-Ai Anwendung" },
+                onClick = {},
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary,
+                )
+            ) { Text("App Starten", modifier = Modifier.clearAndSetSemantics{}) }
+        }
+    }
 
 }
 
 @Preview(showBackground = true, name = "EyeAI-Preview")
 @Composable
-fun EyeAIPreview(){
-    MaterialTheme {WelcomePage(modifier = Modifier.fillMaxSize()) }
+fun EyeAIPreview() {
+    MaterialTheme { WelcomePage(modifier = Modifier.fillMaxSize()) }
 
 }
