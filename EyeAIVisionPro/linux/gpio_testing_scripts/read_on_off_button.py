667e6a3065df9c4fbd2272b75781fd690af70d02
hyperpixel34 <min.ecraft@outlook.de>
2026-08-07 15:36:54 +0200

    gpio test: button tester added

--- EyeAIVisionPro/linux/gpio_testing_scripts/read_on_off_button.py
+++ EyeAIVisionPro/linux/gpio_testing_scripts/read_on_off_button.py
@@ -1,1 +1,21 @@
-print("Hallo Welt")
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Test script to check the functionality of the On-Off Button on the HAT
+
+BUTTON_PIN = 22
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(BUTTON_PIN, GPIO.IN)
+
+for i in range(30):
+    if GPIO.input(BUTTON_PIN):
+        state = "Released"
+    else:
+        state = "Pressed"
+
+    print(f"Button status ({i}): {state}")
+    sleep(0.5)
+
+GPIO.cleanup()
+
