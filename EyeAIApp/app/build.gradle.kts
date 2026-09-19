09771cb6b32dd8c3a1bfcf2ec11dd4bf23614069
Peanutt42 <p3anutt42@gmail.com>
2026-09-19 02:12:10 +0200

    chore: eye-ai-core-rs version 0.1.0 -> 1.0.0, EyeAIApp version 1.0.0 -> 2.0.0

--- EyeAIApp/app/build.gradle.kts
+++ EyeAIApp/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
 		applicationId = "com.algorithmic_alliance.eyeaiapp"
 		minSdk = 26
 		targetSdk = 38
-		versionCode = 1
-		versionName = "1.0"
+		versionCode = 2
+		versionName = "2.0"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
 		ndk {
